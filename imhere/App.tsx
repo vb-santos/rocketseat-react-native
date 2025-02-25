39ed402f86f6f3f6fe58220435e5163998bde3c0
victorb-s <victorb.santos15@gmail.com>
2025-02-25 00:22:16 -0300

    Módulo 01 - Fundamentos - Aula 10

--- imhere/App.tsx
+++ imhere/App.tsx
@@ -1,12 +1,30 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View } from 'react-native'
 
 const App = () => {
-  
-  return (<>
-    <Text>React Native</Text>
-    <Text>Primeira Aula</Text>
-  </>)
+  return (
+    <View style={{
+      flex: 1,
+      backgroundColor: '#131016',
+      padding: 24,
+    }}>
+      <Text style={{
+        color: '#FDFCFE',
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 48
+      }}>
+        Nome do evento
+      </Text>
+
+      <Text style={{
+        color: '#6B6B6B',
+        fontSize: 16,
+      }}>
+        Terça, 25 de Fevereiro de 2025.
+      </Text>
+    </View>
+  )
 }
 
 export default App
