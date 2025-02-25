4e66a72dbeb72723510424e9bf448479070ffe5e
victorb-s <victorb.santos15@gmail.com>
2025-02-25 00:06:42 -0300

    Módulo 01 - Fundamentos - Aula 08

---
+++ imhere/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const App = () => {
+  
+  return (<>
+    <Text>React Native</Text>
+    <Text>Primeira Aula</Text>
+  </>)
+}
+
+export default App
