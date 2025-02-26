fc41d3b3a7fdfcb2d0572622d25b5b01c32a3050
victorb-s <victorb.santos15@gmail.com>
2025-02-26 18:09:34 -0300

    Módulo 01 - Fundamentos - Aula 12

--- imhere/App.tsx
+++ imhere/App.tsx
@@ -1,30 +1,38 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 
 const App = () => {
   return (
-    <View style={{
-      flex: 1,
-      backgroundColor: '#131016',
-      padding: 24,
-    }}>
-      <Text style={{
-        color: '#FDFCFE',
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginTop: 48
-      }}>
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
         Nome do evento
       </Text>
 
-      <Text style={{
-        color: '#6B6B6B',
-        fontSize: 16,
-      }}>
+      <Text style={styles.eventDate}>
         Terça, 25 de Fevereiro de 2025.
       </Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#131016',
+    padding: 24,
+  },
+
+  eventName: {
+    color: '#FDFCFE',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48
+  },
+
+  eventDate: {
+    color: '#6B6B6B',
+    fontSize: 16,
+  }
+})
 
 export default App
