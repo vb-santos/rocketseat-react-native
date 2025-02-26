7fbedd7bd35d28c5cbb921c3d34632abfa0f330c
victorb-s <victorb.santos15@gmail.com>
2025-02-26 20:12:07 -0300

    Fundamentos M01-A16: touchableOpacity

--- imhere/src/screens/Home/index.tsx
+++ imhere/src/screens/Home/index.tsx
@@ -1,7 +1,11 @@
-import { Text, View, TextInput } from 'react-native'
+import { Text, View, TextInput, TouchableOpacity } from 'react-native'
 import { styles } from './styles'
 
 const Home = () => {
+  const handleParticipantAdd = () => {
+    console.log('Você clicou no botão de adicionar!');
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>
@@ -17,6 +21,15 @@ const Home = () => {
         placeholder="Nome do participante"
         placeholderTextColor="#6B6B6B"
       />
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={handleParticipantAdd}
+      >
+        <Text style={styles.buttonText}>
+          +
+        </Text>
+      </TouchableOpacity>
     </View>
   )
 }
--- imhere/src/screens/Home/styles.ts
+++ imhere/src/screens/Home/styles.ts
@@ -23,8 +23,26 @@ export const styles = StyleSheet.create({
     backgroundColor: '#1F1E25',
     height: 56,
     borderRadius: 5,
+
     color: '#FFF',
     padding: 16,
     fontSize: 16,
-  }
+  },
+
+  buttonText: {
+    color: "#FFF",
+    fontSize: 24,
+  },
+
+  button: {
+    width: 56,
+    height: 56,
+    backgroundColor: '#31CF67',
+    borderRadius: 5,
+    
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  
 });
