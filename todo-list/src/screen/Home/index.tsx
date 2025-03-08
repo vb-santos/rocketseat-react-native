7571861d69a949fc6dee1e168ec54627e8c1f3ab
victorb-s <victorb.santos15@gmail.com>
2025-03-08 16:21:34 -0300

    P.P. 07 - Implementação do contador de tasks e lista de tasks

--- todo-list/src/screen/Home/index.tsx
+++ todo-list/src/screen/Home/index.tsx
@@ -1,4 +1,4 @@
-import { View, Image } from 'react-native'
+import { View, Image, Text, FlatList } from 'react-native'
 import { useState } from 'react'
 
 import { Input } from '../../components/Input'
@@ -8,6 +8,9 @@ import { styles } from './styles'
 const Home = () => {
   const [checked, setChecked] = useState<boolean>(false);
   const [isPressed, setIsPressed] = useState<boolean>(false);
+
+  const [counterCreated, setCounterCreated] = useState<number>(0);
+  const [counterDone, setCounterDone] = useState<number>(0);
 
   const toggleChecked = () => {
     setChecked(!checked);
@@ -31,6 +34,33 @@ const Home = () => {
         isPressed={isPressed}
         handlePressIn={handlePressIn}
         handlePressOut={handlePressOut}
+      />
+
+      <View style={styles.amountContainer}>
+        <View style={styles.amountRow}>
+          <Text style={styles.created}>Criadas</Text>
+          <Text style={styles.amount}>{counterCreated}</Text>
+        </View>
+
+        <View style={styles.amountRow}>
+          <Text style={styles.done}>Concluídas</Text>
+          <Text style={styles.amount}>{counterDone}</Text>
+        </View>
+      </View>
+      <View style={styles.line}></View>
+
+      <FlatList
+        data={null}
+        keyExtractor={item => item}
+        renderItem={({ item }) => null}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <View style={styles.listEmptyContainer}>
+            <Image source={require('../../../assets/Clipboard.png')}/>
+            <Text style={styles.listEmptyTitle}>Você ainda não tem tarefas cadastradas</Text>
+            <Text style={styles.listEmptyText}>Crie tarefas e organize seus itens a fazer</Text>
+          </View>
+        )}
       />
     </View>
   </>)
--- todo-list/src/screen/Home/styles.ts
+++ todo-list/src/screen/Home/styles.ts
@@ -16,4 +16,70 @@ export const styles = StyleSheet.create({
 
     alignItems: 'center',
   },
+
+  amountContainer: {
+    width: '90%',
+    height: 48,
+
+    marginTop: 32,
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  amountRow: {
+    flexDirection: 'row',
+    gap: 8,
+  },
+
+  created: {
+    color: theme.colors.blue,
+    fontSize: 14,
+    fontFamily: theme.fonts.title,
+  },
+
+  done: {
+    color: theme.colors.purple,
+    fontSize: 14,
+    fontFamily: theme.fonts.title,
+  },
+
+  amount: {
+    color: theme.colors.gray_200,
+    fontSize: 14,
+    fontFamily: theme.fonts.title,
+
+    backgroundColor: theme.colors.gray_400,
+    borderRadius: 12,
+
+    width: 25,
+    height: 19,
+
+    textAlign: 'center',
+  },
+
+  line: {
+    width: '90%',
+    height: 1,
+    backgroundColor: theme.colors.gray_400,
+  },
+
+  listEmptyContainer: {
+    marginTop: 32,
+    alignItems: 'center',
+  },
+
+  listEmptyTitle: {
+    marginTop: 12,
+
+    color: theme.colors.gray_300,
+    fontSize: 14,
+    fontFamily: theme.fonts.title,
+  },
+
+  listEmptyText: {
+    color: theme.colors.gray_300,
+    fontSize: 14,
+    fontFamily: theme.fonts.text,
+  },
 })
