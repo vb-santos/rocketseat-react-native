f9f7a387d110a7553cba56f2c298378d4793132b
victorb-s <victorb.santos15@gmail.com>
2025-03-12 19:49:23 -0300

    Estrutura M02-A04: Organizando os arquivos do projeto

---
+++ igniteteams/src/Groups/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import { styles } from "./styles";
+
+const Groups = () => {
+  return (
+    <View style={styles.container}>
+      <Text>CAVALO</Text>
+    </View>
+  )
+}
+
+export default Groups
