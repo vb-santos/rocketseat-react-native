d7d136193cf15b4cb89ecab0ca5210584776d44d
victorb-s <victorb.santos15@gmail.com>
2025-03-19 23:33:01 -0300

    CSS-in-JS M02-A07: useFonts e loader

--- igniteteams/App.tsx
+++ igniteteams/App.tsx
@@ -1,6 +1,17 @@
+import { ActivityIndicator } from "react-native";
 import Groups from "@screens/Groups";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 
 const App = () => {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator />
+  }
+
   return (
     <Groups />
   )
