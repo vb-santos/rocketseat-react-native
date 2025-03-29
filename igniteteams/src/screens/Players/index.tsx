0230dcc7a12eea66d61a5095e9314b436bd75c96
victorb-s <victorb.santos15@gmail.com>
2025-03-29 12:30:32 -0300

    CSS-in-JS M02-A29: Finalizar Players list

--- igniteteams/src/screens/Players/index.tsx
+++ igniteteams/src/screens/Players/index.tsx
@@ -4,14 +4,17 @@ import { FlatList } from "react-native";
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
 import { Input } from "@components/Input";
+import { Button } from "@components/Button";
 import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
+import { PlayerCard } from "@components/PlayerCard";
+import { ListEmpty } from "@components/ListEmpty";
 
 const Players = () => {
   const [team, setTeam] = useState('Time A');
-  const [players, setPlayers] = useState([]);
+  const [players, setPlayers] = useState(['Victor', 'Rodrigo']);
 
   return (
     <Container>
@@ -51,6 +54,32 @@ const Players = () => {
           {players.length}
         </NumberOfPlayers>
       </HeaderList>
+
+      <FlatList 
+        data={players}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <PlayerCard
+            name={item}
+            onRemove={() => { }}
+          />
+        )}
+        ListEmptyComponent={() => (
+          <ListEmpty 
+            message="Não há pessoas nesse time."
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 }
+        ]}
+      />
+
+      <Button
+        title="Remover Turma"
+        type="SECONDARY"
+      />
     </Container>
   )
 }
