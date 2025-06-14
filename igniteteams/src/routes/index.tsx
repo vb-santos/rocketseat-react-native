1c8a1c732cc8db43ffbeafe2258662e7b3f0ac37
victorb-s <victorb.santos15@gmail.com>
2025-06-13 21:54:37 -0300

    Navegação 06: Remover efeito de piscagem entre telas

--- igniteteams/src/routes/index.tsx
+++ igniteteams/src/routes/index.tsx
@@ -1,11 +1,15 @@
+import { View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 
 import { AppRoutes } from "./app.routes";
+import { theme } from "@theme/index";
 
 export const Routes = () => {
   return (
-    <NavigationContainer>
-      <AppRoutes />
-    </NavigationContainer>
+    <View style={{ flex: 1, backgroundColor: theme.COLORS.GRAY_600}}>
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
+    </View>
   )
 }
