708ca02a95e39f999ad3bd5d0c24eac304df3b66
victorb-s <victorb.santos15@gmail.com>
2025-06-14 12:01:47 -0300

    Local Storage 03: Criando e utilizando exceptions personalizadas

--- igniteteams/src/screens/NewGroup/index.tsx
+++ igniteteams/src/screens/NewGroup/index.tsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
+import { Alert } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 import { Container, Content, Icon } from "./styles";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
-import { Input } from "@components/Input";
 
+import { Input } from "@components/Input";
 import { groupCreate } from "@storage/group/groupCreate";
-import {groupsGetAll} from "@storage/group/groupsGetAll";
+import { AppError } from "@utils/AppError";
 
 const NewGroup = () => {
   const [group, setGroup] = useState("");
@@ -16,10 +17,19 @@ const NewGroup = () => {
 
   const handleNew = async () => {
     try {
+      if (group.trim().length === 0) {
+        return Alert.alert("Novo Grupo", "Informe o nome da turma.");
+      }
+
       await groupCreate(group)
       navigation.navigate("players", { group });
     } catch (error) {
-      console.log(error)
+      if (error instanceof AppError) {
+        Alert.alert("Novo Grupo", error.message);
+      } else {
+        Alert.alert("Novo Grupo", "Não foi possível criar um novo grupo.");
+        console.log(error);
+      }
     }
   }
 
--- igniteteams/src/storage/group/groupCreate.ts
+++ igniteteams/src/storage/group/groupCreate.ts
@@ -1,12 +1,20 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import { GROUP_COLLECTION } from "@storage/storageConfig";
-import {groupsGetAll} from "@storage/group/groupsGetAll";
+import { groupsGetAll } from "@storage/group/groupsGetAll";
+
+import { AppError } from "@utils/AppError";
 
 export const groupCreate = async (newGroup: string) => {
   try {
-    const storedGroups = await groupsGetAll()
+    const storedGroups = await groupsGetAll();
 
-    const storage = JSON.stringify([...storedGroups, newGroup])
+    const groupAlreadyExists = storedGroups.includes(newGroup);
+    if (groupAlreadyExists) {
+      throw new AppError("Já existe um grupo cadastrado com esse nome.");
+    }
+
+    const storage = JSON.stringify([...storedGroups, newGroup]);
     await AsyncStorage.setItem(GROUP_COLLECTION, storage);
   } catch(error) {
     throw error;
