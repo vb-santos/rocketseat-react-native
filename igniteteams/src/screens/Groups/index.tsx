8472e3a56a4d10eca735f05bf546416de628552a
victorb-s <victorb.santos15@gmail.com>
2025-06-14 11:35:57 -0300

    Local Storage 02: UseFocusEffect

--- igniteteams/src/screens/Groups/index.tsx
+++ igniteteams/src/screens/Groups/index.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { FlatList } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
 
 import { Container } from "./styles";
 
@@ -10,6 +10,8 @@ import { GroupCard } from "@components/GroupCard";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
 
+import { groupsGetAll } from "@storage/group/groupsGetAll";
+
 const Groups = () => {
   const [groups, setGroups] = useState<string[]>([])
   const navigation = useNavigation();
@@ -17,6 +19,19 @@ const Groups = () => {
   const handleNewGroup = () => {
     navigation.navigate("new");
   }
+
+  const fetchGroups = async () => {
+    try {
+      const data = await groupsGetAll()
+      setGroups(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useFocusEffect(useCallback(() => {
+    fetchGroups();
+  }, []));
 
   return (
     <Container>
--- igniteteams/src/screens/NewGroup/index.tsx
+++ igniteteams/src/screens/NewGroup/index.tsx
@@ -7,12 +7,20 @@ import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 
+import { groupCreate } from "@storage/group/groupCreate";
+import {groupsGetAll} from "@storage/group/groupsGetAll";
+
 const NewGroup = () => {
   const [group, setGroup] = useState("");
   const navigation = useNavigation()
 
-  const handleNew = () => {
-    navigation.navigate("players", { group });
+  const handleNew = async () => {
+    try {
+      await groupCreate(group)
+      navigation.navigate("players", { group });
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
@@ -22,7 +30,7 @@ const NewGroup = () => {
       <Content>
         <Icon />
 
-        <Highlight 
+        <Highlight
           title="Nova turma"
           subtitle="Crie a turma para adicionar as pessoas"
         />
@@ -32,7 +40,7 @@ const NewGroup = () => {
           onChangeText={text => setGroup(text)}
         />
 
-        <Button 
+        <Button
           title="Criar"
           style={{ marginTop: 20 }}
           onPress={handleNew}
