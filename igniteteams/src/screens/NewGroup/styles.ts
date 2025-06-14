e54dd319624777fa112e14cbbe019e48c527a020
victorb-s <victorb.santos15@gmail.com>
2025-06-13 21:39:23 -0300

    Navegação 04: Utilizando SafeAreaView

--- igniteteams/src/screens/NewGroup/styles.ts
+++ igniteteams/src/screens/NewGroup/styles.ts
@@ -1,9 +1,10 @@
+import { SafeAreaView } from "react-native-safe-area-context"
 import styled from "styled-components/native";
 import { UsersThree } from "phosphor-react-native"
 
 import { theme } from "@theme/index";
 
-export const Container = styled.View`
+export const Container = styled(SafeAreaView)`
   flex: 1;
   background-color: ${theme.COLORS.GRAY_600};
   padding: 24px;
