56923bd0990479f554db7c6ce1900782f4b01c61
victorb-s <victorb.santos15@gmail.com>
2025-06-27 20:31:58 -0300

    Ignite Gym 06: Finalizando a SignIn Page

--- ignitegym/src/screens/SignIn/index.tsx
+++ ignitegym/src/screens/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import {VStack, Image, Center, Text, Heading } from "@gluestack-ui/themed";
+import {VStack, Image, Center, Text, Heading, ScrollView } from "@gluestack-ui/themed";
 
 import BackgroundImg from "@assets/background.png";
 import Logo from "@assets/logo.svg";
@@ -8,58 +8,63 @@ import { Button } from "@components/Button";
 
 export const SignIn = () => {
   return (
-    <VStack flex={1} bg="$gray700">
-      <Image
-        w="$full"
-        h={624}
-        position="absolute"
-        source={BackgroundImg}
-        defaultSource={BackgroundImg}
-        alt="Pessoas treinando na academia no fundo da página"
-      />
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} bg="$gray700">
+        <Image
+          w="$full"
+          h={624}
+          position="absolute"
+          source={BackgroundImg}
+          defaultSource={BackgroundImg}
+          alt="Pessoas treinando na academia no fundo da página"
+        />
 
-      <VStack flex={1} px="$10" pb="$16">
-        <Center my="$24">
-          <Logo />
+        <VStack flex={1} px="$10" pb="$16">
+          <Center my="$24">
+            <Logo />
 
-          <Text color="$gray100" fontSize="$sm">
-            Treine sua mente e o seu corpo.
-          </Text>
-        </Center>
+            <Text color="$gray100" fontSize="$sm">
+              Treine sua mente e o seu corpo.
+            </Text>
+          </Center>
 
-        <Center gap="$2">
-          <Heading color="$gray100">
-            Acesse a sua conta
-          </Heading>
+          <Center gap="$2">
+            <Heading color="$gray100">
+              Acesse a sua conta
+            </Heading>
 
-          <Input
-            placeholder="E-mail"
-            keyboardType="email-address"
-            autoCapitalize="none"
-          />
-          <Input
-            placeholder="Senha"
-            secureTextEntry
-          />
+            <Input
+              placeholder="E-mail"
+              keyboardType="email-address"
+              autoCapitalize="none"
+            />
+            <Input
+              placeholder="Senha"
+              secureTextEntry
+            />
 
-          <Button
-            title="Acessar"
-          />
-        </Center>
+            <Button
+              title="Acessar"
+            />
+          </Center>
 
-        <Center flex={1} justifyContent="flex-end" mt="$4">
-          <Text
-            color="$gray100"
-            fontSize="$sm"
-            mb="$3"
-            fontFamily="$body"
-          >
-            Ainda não tem acesso?
-          </Text>
+          <Center flex={1} justifyContent="flex-end" mt="$4">
+            <Text
+              color="$gray100"
+              fontSize="$sm"
+              mb="$3"
+              fontFamily="$body"
+            >
+              Ainda não tem acesso?
+            </Text>
 
-          <Button title="Criar Conta" variant="outline" />
-        </Center>
+            <Button title="Criar Conta" variant="outline" />
+          </Center>
+        </VStack>
       </VStack>
-    </VStack>
+    </ScrollView>
   )
 }
