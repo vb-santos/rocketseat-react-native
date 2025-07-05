3298189a19f4d9f6cf4bdeafaace729c5692dea4
victorb-s <victorb.santos15@gmail.com>
2025-07-05 11:49:09 -0300

    Ignite Gym 18: Screen Profile e componentes finalizados

--- ignitegym/src/components/Input/index.tsx
+++ ignitegym/src/components/Input/index.tsx
@@ -2,23 +2,27 @@ import { ComponentProps } from "react";
 import { Input as GSInput, InputField } from "@gluestack-ui/themed";
 
 
-type Props = ComponentProps<typeof InputField>
+type Props = ComponentProps<typeof InputField> & {
+  isReadOnly?: boolean
+}
 
 
-export const Input = ({ ...rest }: Props) => {
+export const Input = ({ isReadOnly=false, ...rest }: Props) => {
   return (
     <GSInput
-      bg="$gray700"
       h="$14"
-      px="$4"
       borderWidth="$0"
       borderRadius="$md"
       $focus={{
         borderWidth: 1,
         borderColor: "$green500",
       }}
+      isReadOnly={isReadOnly}
+      opacity={isReadOnly ? 0.5 : 1}
     >
       <InputField
+        px="$4"
+        bg="$gray700"
         color="$white"
         fontFamily="$body"
         placeholderTextColor="$gray300"
--- ignitegym/src/screens/Profile/index.tsx
+++ ignitegym/src/screens/Profile/index.tsx
@@ -1,9 +1,61 @@
-import { Center, Text } from "@gluestack-ui/themed";
+import { ScrollView, TouchableOpacity } from "react-native";
+import { Center, Heading, Text, VStack } from "@gluestack-ui/themed";
+
+import { ScreenHeader } from "@components/ScreenHeader";
+import { UserPhoto } from "@components/UserPhoto";
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
 
 export const Profile = () => {
   return (
-    <Center flex={1}>
-      <Text color="$white">Profile</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+        <Center mt="$6" px="$10">
+          <UserPhoto
+            source={{ uri: "https://github.com/victorb-s.png" }}
+            alt="Foto de perfil do usuário"
+            size="xl"
+          />
+
+          <TouchableOpacity>
+            <Text
+              color="$green500"
+              fontFamily="$heading"
+              fontSize="$md"
+              mt="$2"
+              mb="$8"
+            >
+              Alterar Foto
+            </Text>
+          </TouchableOpacity>
+
+          <Center w="$full" gap="$4">
+            <Input placeholder="Nome" bg="$gray600" />
+            <Input value="victorb.santos15@gmail.com" bg="$gray600" isReadOnly />
+          </Center>
+
+          <Heading
+            alignSelf="flex-start"
+            fontFamily="$heading"
+            color="$gray200"
+            fontSize="$md"
+            mt="$12"
+            mb="$2"
+          >
+            Alterar senha
+          </Heading>
+
+          <Center w="$full" gap="$4">
+            <Input placeholder="Senha antiga" bg="$gray600" secureTextEntry />
+            <Input placeholder="Nova senha" bg="$gray600" secureTextEntry />
+            <Input placeholder="Confirmar senha" bg="$gray600" secureTextEntry />
+
+            <Button title="Atualizar" />
+          </Center>
+        </Center>
+      </ScrollView>
+    </VStack>
   )
 }
