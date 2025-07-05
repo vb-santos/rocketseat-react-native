543455045ed710bac39027434fe385224dea25e5
victorb-s <victorb.santos15@gmail.com>
2025-07-05 11:25:50 -0300

    Ignite Gym 17: Screen History e componentes finalizados

---
+++ ignitegym/src/components/HistoryCard/index.tsx
@@ -0,0 +1,40 @@
+import { HStack, VStack, Heading, Text } from "@gluestack-ui/themed";
+
+export const HistoryCard = () => {
+  return (
+    <HStack
+      w="$full"
+      px="$5"
+      py="$4"
+      mb="$3"
+      bg="$gray600"
+      rounded="$md"
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <VStack mr="$5">
+        <Heading
+          color="$white"
+          fontSize="$md"
+          textTransform="capitalize"
+          fontFamily="$heading"
+        >
+          Pernas
+        </Heading>
+
+        <Text
+          color="$gray100"
+          fontSize="$lg"
+          numberOfLines={1}
+        >
+          Cadeira Flexora
+        </Text>
+      </VStack>
+
+      <Text
+        color="$gray300"
+        fontSize="$md"
+      >08:59</Text>
+    </HStack>
+  )
+}
--- ignitegym/src/screens/History/index.tsx
+++ ignitegym/src/screens/History/index.tsx
@@ -1,9 +1,53 @@
-import { Center, Text } from "@gluestack-ui/themed";
+import { useState } from "react";
+import { SectionList } from "react-native";
+import { Heading, Text, VStack } from "@gluestack-ui/themed";
+
+import { ScreenHeader } from "@components/ScreenHeader";
+import { HistoryCard } from "@components/HistoryCard";
 
 export const History = () => {
+  const [exercises, setExercise] = useState([
+    {
+      title: "24.07.2025",
+      data: ["Puxada frontal", "Remada unilateral"],
+    },
+
+    {
+      title: "25.07.2025",
+      data: ["Puxada frontal"],
+    },
+  ]);
+
   return (
-    <Center flex={1}>
-      <Text color="$white">History</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Histórico de Exercícios" />
+      <SectionList
+        sections={exercises}
+        keyExtractor={(item) => item}
+        renderItem={() => <HistoryCard />}
+        renderSectionHeader={({ section }) => (
+          <Heading
+            color="$gray200"
+            fontSize="$md"
+            fontFamily="$heading"
+            mt="$10"
+            mb="$3"
+          >
+            {section.title}
+          </Heading>
+        )}
+        style={{ paddingHorizontal: 32 }}
+        contentContainerStyle={
+          exercises.length === 0 && { flex: 1, justifyContent: "center" }
+        }
+        ListEmptyComponent={() => (
+          <Text color="$gray100" textAlign="center">
+            Não há exercícios registrados ainda. {"\n"}
+            Vamos fazer exercícios hoje?
+          </Text>
+        )}
+        showsVerticalScrollIndicator={false}
+      />
+    </VStack>
   )
 }
