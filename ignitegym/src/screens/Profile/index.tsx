da767259527898832f5e0059775db94aec5856ea
victorb-s <victorb.santos15@gmail.com>
2025-09-08 11:58:16 -0300

    Ignite Gym 19: Galeria de fotos e Toast finalizados

--- ignitegym/src/screens/Profile/index.tsx
+++ ignitegym/src/screens/Profile/index.tsx
@@ -1,12 +1,60 @@
+import { useState } from "react";
 import { ScrollView, TouchableOpacity } from "react-native";
-import { Center, Heading, Text, VStack } from "@gluestack-ui/themed";
+import { Center, Heading, Text, VStack, useToast } from "@gluestack-ui/themed";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
 
+import { ToastMessage } from "@components/ToastMessage";
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
 export const Profile = () => {
+  const [userPhoto, setUserPhoto] = useState("https://github.com/victorb-s.png");
+  
+  const toast = useToast();
+
+  const handleUserPhotoSelect = async () => {
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ['images'],
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      })
+
+      if (photoSelected.canceled) {
+        return toast.show({
+          placement: "top",
+          render: ({ id }) => (
+            <ToastMessage
+              id={id}
+              action="error"
+              title="Tamanho da imagem muito grande!"
+              description="Essa imagem é muito grande, escolha uma de até 5MB."
+              onClose={() => toast.close(id)}
+            />
+          )
+        });
+      }
+
+      const photoURI = photoSelected.assets[0].uri
+
+      if (photoURI) {
+        const photoInfo = await FileSystem.getInfoAsync(photoURI) as { size: number };
+
+        if (photoInfo.size && (photoInfo.size / 1024 / 1024) > 5) {
+          return 
+        }
+
+        setUserPhoto(photoURI);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
@@ -14,12 +62,12 @@ export const Profile = () => {
       <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt="$6" px="$10">
           <UserPhoto
-            source={{ uri: "https://github.com/victorb-s.png" }}
+            source={{ uri: userPhoto }}
             alt="Foto de perfil do usuário"
             size="xl"
           />
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text
               color="$green500"
               fontFamily="$heading"
