e0c786f52427efa78013650475cc7d721c07ef45
victorb-s <victorb.santos15@gmail.com>
2025-09-08 19:47:07 -0300

    Ignite Gym 24: Implementando Yup

--- ignitegym/src/screens/SignUp/index.tsx
+++ ignitegym/src/screens/SignUp/index.tsx
@@ -1,6 +1,8 @@
 import { VStack, Image, Center, Text, Heading, ScrollView } from "@gluestack-ui/themed";
 import { useNavigation } from "@react-navigation/native";
 import { useForm, Controller } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup"
 
 import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
 
@@ -15,12 +17,28 @@ type FormDataProps = {
   name: string;
   email: string;
   password: string;
-  confirm_password: string;
+  confirmPassword: string;
 }
 
 
+const signUpSchema = yup.object({
+  name: yup.string().required("Informe o nome"),
+  email: yup.string().required("Informe o email").email("E-mail inválido"),
+  password: yup
+    .string()
+    .required("Informe a senha")
+    .min(6, "A senha deve ter no mínimo 6 dígitos"),
+  confirmPassword: yup
+    .string()
+    .required("Informe a senha novamente")
+    .oneOf([yup.ref("password"), ""], "A confirmação da senha não confere"),
+});
+
+
 export const SignUp = () => {
-  const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>();
+  const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
+    resolver: yupResolver(signUpSchema)
+  });
 
   const navigation = useNavigation<AuthNavigatorRoutesProps>()
 
@@ -64,9 +82,6 @@ export const SignUp = () => {
             <Controller
               control={control}
               name="name"
-              rules={{
-                required: "Informe o nome"
-              }}
               render={({ field: { onChange, value } }) => (
                 <Input
                   placeholder="Nome"
@@ -80,13 +95,6 @@ export const SignUp = () => {
             <Controller
               control={control}
               name="email"
-              rules={{
-                required: "Informe o email",
-                pattern: {
-                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                  message: "Email inválido!"
-                }
-              }}
               render={({ field: { onChange, value } }) => (
                 <Input
                   placeholder="E-mail"
@@ -108,19 +116,21 @@ export const SignUp = () => {
                   secureTextEntry
                   onChangeText={onChange}
                   value={value}
+                  errorMessage={errors.password?.message}
                 />
               )}
             />
 
             <Controller
               control={control}
-              name="confirm_password"
+              name="confirmPassword"
               render={({ field: { onChange, value } }) => (
                 <Input
                   placeholder="Confirme a Senha"
                   secureTextEntry
                   onChangeText={onChange}
                   value={value}
+                  errorMessage={errors.confirmPassword?.message}
                   onSubmitEditing={handleSubmit(handleSignUp)}
                   returnKeyType="send"
                 />
