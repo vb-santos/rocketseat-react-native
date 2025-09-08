1fc54415dc3310efff3e537222a3abaf71e2986b
victorb-s <victorb.santos15@gmail.com>
2025-09-08 14:51:34 -0300

    Ignite Gym 20: Inputs com useState

--- ignitegym/src/screens/SignUp/index.tsx
+++ ignitegym/src/screens/SignUp/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { VStack, Image, Center, Text, Heading, ScrollView } from "@gluestack-ui/themed";
 import { useNavigation } from "@react-navigation/native";
 
@@ -10,10 +11,24 @@ import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
 export const SignUp = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
   const navigation = useNavigation<AuthNavigatorRoutesProps>()
 
   const handleGoBack = () => {
     navigation.navigate("signIn");
+  }
+
+  const handleSignUp = () => {
+    console.log({
+      name,
+      email,
+      password,
+      confirmPassword,
+    })
   }
 
   return (
@@ -45,19 +60,33 @@ export const SignUp = () => {
               Crie sua conta
             </Heading>
 
-            <Input placeholder="Nome" />
+            <Input
+              placeholder="Nome"
+              onChangeText={setName}
+            />
+
             <Input
               placeholder="E-mail"
               keyboardType="email-address"
               autoCapitalize="none"
+              onChangeText={setEmail}
             />
+
             <Input
               placeholder="Senha"
               secureTextEntry
+              onChangeText={setPassword}
+            />
+
+            <Input
+              placeholder="Confirme a Senha"
+              secureTextEntry
+              onChangeText={setConfirmPassword}
             />
 
             <Button
               title="Criar e acessar"
+              onPress={handleSignUp}
             />
           </Center>
 
