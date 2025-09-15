ce952be4985eba652c0561c170b6f9123ed97ccf
victorb-s <victorb.santos15@gmail.com>
2025-09-15 10:47:24 -0300

    Ignite Gym 32: Utilizando AsyncStorage para login persistido

--- ignitegym/frontend/src/contexts/AuthContext.tsx
+++ ignitegym/frontend/src/contexts/AuthContext.tsx
@@ -1,12 +1,14 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
+
+import { storageUserSave, storageUserGet } from "@storage/storageUser";
 
 import { api } from "@services/api";
-
 import { UserDTO } from "@dtos/UserDTO";
 
 export type AuthContextDataProps = {
   user: UserDTO;
   signIn: (email: string, password: string) => Promise<void>;
+  isLoadingUserStorageData: boolean;
 }
 
 type AuthContextProviderProps = {
@@ -17,21 +19,45 @@ export const AuthContext = createContext<AuthContextDataProps>({} as AuthContext
 
 export const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
   const [user, setUser] = useState<UserDTO>({} as UserDTO);
+  const [isLoadingUserStorageData, setIsLoadingUserStorageData] = useState(true);
 
   const signIn = async (email: string, password: string) => {
-    try{
+    try {
       const { data } = await api.post("/sessions", { email, password });
 
-      if(data.user) {
+      if (data.user) {
         setUser(data.user);
+        await storageUserSave(data.user);
       }
     } catch (error){
       throw error;
     }
   }
 
+  const loadUserData = async () => {
+    try {
+      const userLogged = await storageUserGet()
+
+      if (userLogged) {
+        setUser(userLogged);
+      }
+    } catch (error) {
+      throw error;
+    } finally {
+      setIsLoadingUserStorageData(false);
+    }
+  }
+
+  useEffect(() => {
+    loadUserData();
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ user, signIn }}>
+    <AuthContext.Provider value={{
+      user,
+      signIn,
+      isLoadingUserStorageData
+    }}>
       {children}
     </AuthContext.Provider>
   )
--- ignitegym/frontend/src/routes/index.tsx
+++ ignitegym/frontend/src/routes/index.tsx
@@ -1,23 +1,29 @@
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { gluestackUIConfig } from "../../config/gluestack-ui.config"
 import { Box } from "@gluestack-ui/themed";
 
 import { AuthRoutes } from "@routes/auth.routes";
-import { AppRoutes} from "@routes/app.routes";
+
+import { AppRoutes } from "@routes/app.routes";
 
 import { useAuth } from "@hooks/useAuth";
 
-import { gluestackUIConfig } from "../../config/gluestack-ui.config"
+import { Loading } from "@components/Loading";
 
 export const Routes = () => {
   const theme = DefaultTheme
   theme.colors.background = gluestackUIConfig.tokens.colors.gray700
 
-  const { user } = useAuth();
+  const { user, isLoadingUserStorageData } = useAuth();
+
+  if (isLoadingUserStorageData) {
+    return <Loading />
+  }
 
   return (
     <Box flex={1} bg="$gray700">
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        { user.id ? <AppRoutes /> : <AuthRoutes />}
       </NavigationContainer>
     </Box>
   )
